d5814e87f77f5255e2702c52f721d3533efe78c1
KarlDragon <153891903+KarlDragon@users.noreply.github.com>
2024-01-21 12:57:45 +0700

    Completing UI side

--- Main.py
+++ Main.py
@@ -1,45 +1,23 @@
 import os
 import threading
 import tkinter as tk
-from Module_File import Server_UI, Client_UI, First_time_UI, FaceRecognition
+from Module_File import Server_UI, Client_UI, First_time_UI
+from queue import Queue
 
 class AppManager:
     def __init__(self):
         self.is_txt = os.path.exists('information.txt')
 
-    def run_feature(self, path, IP, port):
-        feature = FaceRecognition.FaceRecognition()
-        feature.run_face_recognition(path, IP, port)
+    def run_client(self,path, IP, port):
+        frame_queue = Queue()
 
-    def run_client_ui(self):
-        root = tk.Tk()
-        app = Client_UI.Client_UI(root)
-        app.Cl_UI()
-        root.mainloop()
+        gui_thread = threading.Thread(target=Client_UI.run_gui, args=(frame_queue,path, IP, port,))
+        gui_thread.start()
 
-    def run_server_ui(self):
-        app = Server_UI.Server_UI(2, 2)
-
-    def run_first_time_ui(self):
-        app = First_time_UI.First_time_UI()
-
-    def start_threads(self, mode, path, IP, port):
-        if mode == "Cl":
-            thread_ui = threading.Thread(target=self.run_client_ui)
-            thread_feature = threading.Thread(target=self.run_feature, args=(path, IP, port))
-
-            thread_ui.start()
-            thread_feature.start()
-
-        elif mode == "Sv":
-            thread_server = threading.Thread(target=self.run_server_ui)
-            thread_server.start()
-
-        else:
-            thread_first_time_ui = threading.Thread(target=self.run_first_time_ui)
-            thread_first_time_ui.start()
+        gui_thread.join()
 
 if __name__ == "__main__":
+    
     app_manager = AppManager()
 
     if app_manager.is_txt:
@@ -48,7 +26,8 @@ if __name__ == "__main__":
             path = f.readline().replace('\n', '')
             IP = f.readline().replace('\n', '')
             port = int(f.readline())
-
-        app_manager.start_threads(mode, path, IP, port)
+        if mode=="Cl":
+            app_manager.run_client(path, IP, port)
     else:
-        app_manager.start_threads("", "", "", 0)
+        #app_manager.start_threads("", "", "", 0)
+        pass
