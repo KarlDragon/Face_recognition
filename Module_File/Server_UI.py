a55f137ff14483a03363a68292d9c4c4fd98a07c
KarlDragon <153891903+KarlDragon@users.noreply.github.com>
2024-01-29 19:07:35 +0700

    Update Server_UI.py

--- Module_File/Server_UI.py
+++ Module_File/Server_UI.py
@@ -9,6 +9,8 @@ import threading
 import numpy as np
 import pickle
 import sys
+import queue
+import subprocess
 class UI_UX:
     def __init__(self, root):
         self.root = root
@@ -42,31 +44,8 @@ class Server_UI(UI_UX):
         self.amount=amount
         self.server = Server(self.path, self.IP, self.port,self.amount)
         self.back_ground = False
-        
-
-    def show_folder(self, search, folder_list, canvas):
-        index = int(search.get()) - 1
-
-        # Clear the previous content of the canvas
-        for widget in canvas.winfo_children():
-            widget.destroy()
-
-        # Display the contents of the selected folder
-        folder_list = os.listdir(selected_folder)
-        for i, folder_name in enumerate(folder_list):
-            folder_button = tk.Button(canvas, text=folder_name, width=51, height=2, command=lambda folder=folder_name: self.open_folder(selected_folder, folder), bg="#FF69B4")
-            folder_button.pack(side=tk.TOP, pady=5)
-
-        # Update canvas properties
-        canvas.update_idletasks()
-        canvas.config(scrollregion=canvas.bbox(tk.ALL))
-
-        # Bind the scroll event to the canvas
-        def on_scroll(event):
-            canvas.yview_scroll(-1 * event.delta // 120, "units")
-
-        canvas.bind_all("<MouseWheel>", on_scroll)
-
+        self.image_queue = queue.Queue()
+    
     #functions for home()
 
     def run_clock(self):
@@ -87,28 +66,36 @@ class Server_UI(UI_UX):
                 widget.destroy()
 
         tk.Label(self.frame, text="Nhập thời gian đếm ngược", height=4, width=177, font="large_font", bg="pink").pack(side=tk.TOP)
-        tk.Label(self.frame, text="Giờ: ", height=2, width=10, font="large_font", bg="#FFF0F5").place(x=530,y=160)
-        tk.Label(self.frame, text="Phút: ", height=2, width=10, font="large_font", bg="#FFF0F5").place(x=730,y=160)
-        tk.Label(self.frame, text="Giây: ", height=2, width=10, font="large_font", bg="#FFF0F5").place(x=930,y=160)
-        tk.Label(self.frame, text="Âm thanh cảnh báo", height=2, width=20, font="large_font", bg="#FFF0F5").place(x=1240,y=160)
-        
-        self.time_h = tk.Entry(self.frame, width=10)
-        self.time_h.configure(font=("Arial", 19))  
-        self.time_h.place(x=550,y=190)
 
-        self.time_m = tk.Entry(self.frame, width=10)
-        self.time_m.configure(font=("Arial", 19))  
-        self.time_m.place(x=750,y=190)
+        self.time_frame = tk.Frame(self.frame, bg="#FFF0F5")
+        self.time_frame.pack(side=tk.TOP,anchor=tk.NW,padx=120)
 
-        self.time_s = tk.Entry(self.frame, width=10)
-        self.time_s.configure(font=("Arial", 19))  
-        self.time_s.place(x=950,y=190)
+        tk.Label(self.time_frame, text="Giờ: ", height=2, width=10, font="large_font", bg="#FFF0F5").pack(side=tk.LEFT,padx=10)
+        tk.Label(self.time_frame, text="Phút: ", height=2, width=10, font="large_font", bg="#FFF0F5").pack(side=tk.LEFT)
+        tk.Label(self.time_frame, text="Giây: ", height=2, width=10, font="large_font", bg="#FFF0F5").pack(side=tk.LEFT)
+        tk.Label(self.time_frame, text="Âm thanh cảnh báo", height=2, width=20, font="large_font", bg="#FFF0F5").pack(side=tk.LEFT)
+
+        self.entry_frame = tk.Frame(self.frame, bg="#FFF0F5")
+        self.entry_frame.pack(side=tk.TOP,anchor=tk.NW,padx=130)
+
+        self.time_h = tk.Entry(self.entry_frame, width=5)
+        self.time_h.configure(font=("Arial", 19))
+        self.time_h.pack(side=tk.LEFT,padx=20)
+
+        self.time_m = tk.Entry(self.entry_frame, width=5)
+        self.time_m.configure(font=("Arial", 19))
+        self.time_m.pack(side=tk.LEFT,padx=20)
+
+        self.time_s = tk.Entry(self.entry_frame, width=5)
+        self.time_s.configure(font=("Arial", 19))
+        self.time_s.pack(side=tk.LEFT,padx=30)
+
 
         self.done_button = tk.Button(self.frame, text="Bắt đầu", width=20, height=3, command=self.run_clock, bg="#99FFFF")
-        self.done_button.place(x=750,y=350)
+        self.done_button.pack(side=tk.TOP,pady=50)
 
-        self.sound_button = tk.Button(self.frame, image=self.Sound_on, width=50, height=40, command=self.sound, bg="#99FFFF")       
-        self.sound_button.place(x=1300,y=200)
+        self.sound_button = tk.Button(self.entry_frame, image=self.Sound_on, width=50, height=40, command=self.sound, bg="#99FFFF")       
+        self.sound_button.pack(side=tk.TOP)
         
         if self.is_sound:
             self.sound_button.config(image=self.Sound_on)
@@ -166,17 +153,44 @@ class Server_UI(UI_UX):
         background_path = tk_logo_path=os.path.join(self.current_directory, 'app_img', 'home_bg.jpg')
         self.tk_background = ImageTk.PhotoImage(Image.open(background_path).resize((self.distance_x,self.distance_y)))
         tk.Label(self.frame, image=self.tk_background).pack(side=tk.TOP,fill=tk.BOTH, expand=True)
-        
-    def show_cam(self, key):
-        threading.Thread(target=self.display_image, args=(key,)).start()
 
+    def update_image_in_main_thread(self):
+        try:
+            img = self.image_queue.get_nowait()
+            self.back_ground.configure(image=img)
+            self.back_ground.image = img
+        except queue.Empty:
+            pass
+
+        # Schedule the next update on the main thread after 100 milliseconds
+        self.root.after(200, self.update_image_in_main_thread)
+
+    def show_cam(self, key):
+        try:
+            self.display_image(key)  
+            self.root.after(200, lambda: self.show_cam(key))
+            self.update_image_in_main_thread()
+        except Exception as e:
+            print(f"Error in show_cam: {e}")
+
+            
     def display_image(self, key):
-        cv2.imshow("Received", self.server.image_dict[key])
-        cv2.waitKey(1)
+        try:
+            img = ImageTk.PhotoImage(
+                Image.fromarray(
+                    cv2.cvtColor(self.server.image_dict[key], cv2.COLOR_BGR2RGB)))
+            # Put the image in the queue
+            self.image_queue.put(img)
+
+            # Schedule the next update after 100 milliseconds
+            self.root.after(200, lambda: self.display_image(key))
+        except Exception as e:
+            print(f"Error displaying image: {e}")
             
     def cam(self, cam_num):
         for widget in self.frame.winfo_children():
             widget.destroy()
+            
         tk.Label(self.frame, text="Cam đang chạy", height=2, width=177, bg="pink",font=("Arial", 15)).pack(side=tk.TOP,fill=tk.X)
         tk.Label(self.frame, text="Danh sách máy hoạt động", height=2, width=25, bg="pink",font=("Arial", 15)).pack(side=tk.TOP,anchor=tk.NW)
 
@@ -214,41 +228,65 @@ class Server_UI(UI_UX):
             canvas.yview_scroll(-1 * event.delta // 120, "units")
             canvas.bind_all("<MouseWheel>", on_scroll)
 
-    def history(self,folder_num):
+    def search_folder(self, search, folder_list, canvas):
+        pass
+
+    def show_folder(self, value):
+        subprocess.Popen(['explorer', '/n, /e, ', value], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        self.frame.update()
+
+    def history(self, folder_num):
         for widget in self.frame.winfo_children():
             widget.destroy()
-            
-        tk.Label(self.frame, text="Lịch sử", height=2, width=177, bg="pink",font=("Arial", 15)).place(x=10, y=10)
-        tk.Label(self.frame, text="Nhập số báo danh", height=2, width=40, bg="#FFF0F5",font=("Arial", 15)).place(x=900, y=62)
-        tk.Label(self.frame, text="Danh sách thư mục", height=2, width=35, bg="pink",font=("Arial", 15)).place(x=10, y=50)
-        
+
+        tk.Label(self.frame, text="Lịch sử", height=2, width=177, bg="pink", font=("Arial", 15)).pack(side=tk.TOP, fill=tk.X)
+        tk.Label(self.frame, text="Nhập số báo danh", height=2, width=40, bg="#FFF0F5", font=("Arial", 15)).place(x=900, y=62)
+        tk.Label(self.frame, text="Danh sách thư mục", height=2, width=25, bg="pink", font=("Arial", 15)).pack(side=tk.TOP,
+                                                                                                           anchor=tk.NW)
+
         self.search = tk.Entry(self.frame, width=80)
-        self.search.place(x=1000,y=100)
-        
-        canvas = tk.Canvas(self.frame, bg="pink", height=880)  
-        canvas.place(x=10, y=90)
+        self.search.place(x=1000, y=100)
+
+        canvas = tk.Canvas(self.frame, bg="pink",width=280 )  
+        canvas.pack(side=tk.LEFT,fill=tk.Y,anchor=tk.NW)
 
         scrollbar = tk.Scrollbar(self.frame, orient=tk.VERTICAL, command=canvas.yview)
-        scrollbar.place(x=390, y=90, height=880)
+        scrollbar.pack(side=tk.LEFT,fill=tk.Y,anchor=tk.NW)
 
         canvas.configure(yscrollcommand=scrollbar.set)
 
         cam_frame = tk.Frame(canvas, bg="pink")
         canvas.create_window((0, 0), window=cam_frame, anchor=tk.NW)
 
-        search_button=tk.Button(self.frame, text="Tìm kiếm",height=1, command=lambda: self.show_folder(self.search,folder_list,canvas))
-        search_button.place(x=1500,y=90)
+        search_button = tk.Button(self.frame, text="Tìm kiếm", height=1,
+                                  command=lambda: self.search_folder(self.search, folder_list, canvas))
+        search_button.place(x=1500, y=90)
+
+        self.bg_fr = tk.Frame(self.frame, bg="#FFF0F5")
+        self.bg_fr.pack(side=tk.TOP,fill=tk.BOTH,expand=True)
         
-        cam_frame.update_idletasks()  
+        for key, value in self.server.user_path.items():
+            if not any(btn["text"] == key for btn in cam_frame.winfo_children()):
+                tk.Button(cam_frame,
+                          text=key,
+                          width=35,
+                          height=1,
+                          command=lambda: self.show_folder(value),  # Truyền tham số cho hàm
+                          font=("Arial", 15),
+                          bg="#FF69B4").pack(side=tk.TOP, pady=5)
+
+        cam_frame.update_idletasks()
 
         canvas.config(scrollregion=canvas.bbox(tk.ALL))  # Xác định vùng cuộn
-        
+
         # Bắt sự kiện cuộn để cuộn cả canvas
         def on_scroll(event):
             canvas.yview_scroll(-1 * event.delta // 120, "units")
-            canvas.bind_all("<MouseWheel>", on_scroll)
 
-        
+        canvas.bind_all("<MouseWheel>", on_scroll)
+
+        # Bỏ sự kiện cuộn khi kết thúc hàm
+        canvas.unbind_all("<MouseWheel>")
         
     def SV_UI(self,amount):
         self.Sx_position = -10
@@ -283,6 +321,82 @@ class Server_UI(UI_UX):
         
         threading.Thread(target=self.server.start_server).start()
 
+class VideoThread(threading.Thread):
+    def __init__(self, conn, user_cam, image_dict, shared_lock):
+        super().__init__()
+        self.conn = conn
+        self.user_cam = user_cam
+        self.image_dict = image_dict
+        self.shared_lock = shared_lock
+
+    def run(self):
+        while True:
+            with self.shared_lock:
+                data = b""
+                payload_size = struct.calcsize("L")
+                while len(data) < payload_size:
+                    packet = self.conn.recv(4 * 1024)
+                    if not packet:
+                        break
+                    data += packet
+
+                packed_msg_size = data[:payload_size]
+                data = data[payload_size:]
+                msg_size = struct.unpack("L", packed_msg_size)[0]
+
+                while len(data) < msg_size:
+                    data += self.conn.recv(4 * 1024)
+
+                frame_data = data[:msg_size]
+                data = data[msg_size:]
+
+                frame = pickle.loads(frame_data)
+                self.image_dict[self.user_cam] = frame
+
+class FolderThread(threading.Thread):
+    def __init__(self, conn, folder_path, shared_lock):
+        super().__init__()
+        self.conn = conn
+        self.folder_path = folder_path
+        self.shared_lock = shared_lock
+
+    def run(self):
+        while True:
+            with self.shared_lock:
+                folder_name = self.conn.recv(1024).decode('utf-8')
+                save_folder = os.path.join(self.folder_path, folder_name)
+                print(save_folder)
+                self.create_folder(save_folder)
+
+                size_data = self.conn.recv(4)
+                if not size_data:
+                    break
+
+                size = struct.unpack("!L", size_data)[0]
+
+                image_data = b""
+                while len(image_data) < size:
+                    chunk = self.conn.recv(size - len(image_data))
+                    if not chunk:
+                        break
+                    image_data += chunk
+
+                if not image_data:
+                    break
+
+                image = cv2.imdecode(np.frombuffer(image_data, dtype=np.uint8), cv2.IMREAD_COLOR)
+
+                filename = f"image_{time.time()}.jpg"
+                image_path = os.path.join(save_folder, filename)
+                cv2.imwrite(image_path, image)
+                print(f"Image received and saved as: {image_path}")
+                print(f"Recv: {len(image_data)} bytes")
+
+                self.conn.sendall(b"ACK")
+
+    def create_folder(self, path):
+        os.makedirs(path, exist_ok=True)
+            
 class Server:
     def __init__(self,path,IP,port,amount):
         self.path=path
@@ -291,61 +405,11 @@ class Server:
         self.amount=amount
         self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.image_dict={}
-##    def folder_thread(self,conn,folder_path):
-##        folder_name = conn.recv(1024).decode('utf-8')
-##        save_folder = os.path.join(folder_path, folder_name)
-##        self.create_folder(save_folder)
-##
-##        size_data = conn.recv(4)
-##        if not size_data:
-##            break
-##
-##        size = struct.unpack("!L", size_data)[0]
-##
-##        image_data = b""
-##        while len(image_data) < size:
-##            chunk = conn.recv(size - len(image_data))
-##            if not chunk:
-##                break
-##            image_data += chunk
-##
-##        if not image_data:
-##            break
-##
-##        image = cv2.imdecode(np.frombuffer(image_data, dtype=np.uint8), cv2.IMREAD_COLOR)
-##
-##        filename = f"image_{time.time()}.jpg"
-##        image_path = os.path.join(save_folder, filename)
-##        cv2.imwrite(image_path, image)
-##        print(f"Image received and saved as: {image_path}")
-##        print(f"Recv: {len(image_data)} bytes")
-##
-##        conn.sendall(b"ACK")
-        
-    def video_thread(self,conn,user_folder):
-        while True:
-            data = b""
-            payload_size = struct.calcsize("L")
-            while len(data) < payload_size:
-                packet = conn.recv(4 * 1024)
-                if not packet:
-                    break
-                data += packet
-
-            packed_msg_size = data[:payload_size]
-            data = data[payload_size:]
-            msg_size = struct.unpack("L", packed_msg_size)[0]
-
-            while len(data) < msg_size:
-                data += conn.recv(4 * 1024)
-
-            frame_data = data[:msg_size]
-            data = data[msg_size:]
-
-            frame = pickle.loads(frame_data)
-            self.image_dict[user_folder]=frame
+        self.user_path={}
+        self.shared_lock = threading.Lock()
         
     def handle_client(self, conn, addr):
+        print(conn)
         ip_address, port_number = addr
         print(f"Connection from {addr}")
         
@@ -361,11 +425,20 @@ class Server:
         print(f"Received class: {class_}")
 
         user_folder=f"{name}_{class_}_{num}"
+        user_cam=f"{name}_{class_}"
+        
         folder_path = os.path.join(self.path, user_folder)
         self.create_folder(folder_path)
         
-        self.video_thread(conn,user_folder)
-        #self.folder_thread(conn,folder_path)
+        video_thread = VideoThread(conn, user_cam, self.image_dict, self.shared_lock)
+        folder_thread = FolderThread(conn, folder_path, self.shared_lock)
+        self.user_path[user_folder]=folder_path
+            
+        video_thread.start()
+        folder_thread.start()
+
+        video_thread.join()
+        folder_thread.join()
 
         conn.close()
         print(f"Connection from {addr} closed")
